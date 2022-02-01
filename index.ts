178a67486285352700a740f09dd9359fc14c764c
rycont <reactdev@kakao.com>
2022-02-01 16:15:37 +0900

    fix: url param to post body

--- index.ts
+++ index.ts
@@ -1,49 +1,50 @@
-import express from "express";
-import cors from "cors";
+import express from "express"
+import cors from "cors"
 
-const app = express();
-app.use(cors());
+const app = express()
 
-const topics: Record<string, ((data: string) => void)[]> = {};
+app.use(cors())
+app.use(express.json())
+
+const topics: Record<string, ((data: string) => void)[]> = {}
 
 app.get("/sub/:topic", (req, res) => {
-  const topic = req.params.topic;
+  const topic = req.params.topic
   if (!topics[topic]) {
-    topics[topic] = [];
+    topics[topic] = []
   }
 
   const subFunc = async (data: string) => {
-    topics[topic] = topics[topic].filter((f) => f !== subFunc);
+    topics[topic] = topics[topic].filter((f) => f !== subFunc)
 
     res.json({
       topic,
       data,
-    });
-  };
+    })
+  }
 
-  console.log("New topic registered");
-  topics[topic].push(subFunc);
-});
+  console.log("New topic registered")
+  topics[topic].push(subFunc)
+})
 
-app.get("/pub/:topic/:data", async (req, res) => {
-  const topic = req.params.topic;
-  const data = req.params.data;
+app.post("/pub/:topic", async (req, res) => {
+  const topic = req.params.topic
+  const data = req.body.data
 
-  console.log(topics);
   if (topics[topic]) {
     while (topics[topic].length)
       for (const sub of topics[topic]) {
-        sub(data);
+        sub(data)
       }
   }
 
   res.json({
     topic,
-  });
-});
+  })
+})
 
 app.get("/topics", (req, res) => {
-  res.json(Object.keys(topics));
-});
+  res.json(Object.keys(topics))
+})
 
-app.listen(3080);
+app.listen(3080)
