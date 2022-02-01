3872750f9ffa2b9c533fe1a32cd40f30816be0fa
rycont <reactdev@kakao.com>
2022-02-01 11:07:52 +0900

    pubsub

--- index.ts
+++ index.ts
@@ -1,14 +1,49 @@
-import { Server, WebSocket } from "ws";
+import express from "express";
 
-const server = new Server({ port: 3080 });
-const sockets: WebSocket[] = [];
+const app = express();
 
-server.on("connection", (socket) => {
-  socket.send("Connected :)");
-  sockets.push(socket);
+const topics: Record<string, ((data: string) => void)[]> = {};
 
-  socket.on("message", function (message: string) {
-    console.log(message);
-    sockets.forEach((s) => s.send(message));
+app.get("/sub/:topic", (req, res) => {
+  const topic = req.params.topic;
+  if (!topics[topic]) {
+    topics[topic] = [];
+  }
+
+  const subFunc = async (data: string) => {
+    topics[topic] = topics[topic].filter((f) => f !== subFunc);
+
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
+    res.json({
+      topic,
+      data,
+    });
+  };
+
+  console.log("New topic registered");
+  topics[topic].push(subFunc);
+});
+
+app.get("/pub/:topic/:data", async (req, res) => {
+  const topic = req.params.topic;
+  const data = req.params.data;
+
+  console.log(topics);
+  if (topics[topic]) {
+    while (topics[topic].length)
+      for (const sub of topics[topic]) {
+        sub(data);
+      }
+  }
+
+  res.json({
+    topic,
   });
 });
+
+app.get("/topics", (req, res) => {
+  res.json(Object.keys(topics));
+});
+
+app.listen(3080);
